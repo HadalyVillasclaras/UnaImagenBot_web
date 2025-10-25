83704b5ffd4d1818a797d45618d4535b643f0b4d
Hadaly <hadalyvf@gmail.com>
2025-10-25 21:56:19 +0200

    feat: iso card flip

---
+++ src/js/IsoCard.js
@@ -0,0 +1,84 @@
+export class IsoCard {
+  constructor() {
+    this.card = document.querySelector('.s-iso-card .c-card')
+    this.textElement = this.card.querySelector('.c-card__txt p')
+    this.flipCard = document.querySelector('.s-iso-card.flip .c-card')
+    this.frontText = this.flipCard ? this.flipCard.querySelector('.c-card__front .c-card__txt p') : null
+    this.backText = this.flipCard ? this.flipCard.querySelector('.c-card__back .c-card__txt p') : null
+    this.isFlipped = false
+    this.data = []
+    this.dataIndex = 0
+
+    this.init()
+  }
+
+  async init() {
+    await this.loadData()
+    this.updateText()
+    this.card.addEventListener('click', () => this.handleInteraction())
+
+    if (this.flipCard) {
+      this.frontText.textContent = this.data[this.consumeIndex()]
+      this.backText.textContent = this.data[this.consumeIndex()]
+      this.flipCard.addEventListener('click', () => this.handleFlip())
+    }
+  }
+
+  async loadData() {
+    try {
+      const res = await fetch('/images.json')
+      this.data = await res.json()
+    } catch (err) {
+      console.error('Error loading JSON:', err)
+    }
+  }
+
+  updateText() {
+    const text = this.data[this.dataIndex % this.data.length]
+    this.textElement.textContent = text
+  }
+
+  handleInteraction() {
+    this.animateCard(() => {
+      this.dataIndex = (this.dataIndex + 1) % this.data.length
+      this.updateText()
+    })
+  }
+
+  animateCard(onMidAnimation) {
+    this.card.style.opacity = 0
+    setTimeout(() => {
+      onMidAnimation()
+      this.card.style.opacity = 1
+    }, 800)
+  }
+
+  consumeIndex() {
+    const i = this.dataIndex % this.data.length
+    this.dataIndex = (this.dataIndex + 1) % this.data.length
+    return i
+  }
+
+  handleFlip() {
+    this.isFlipped = !this.isFlipped
+  
+    if (this._flipTimer) clearTimeout(this._flipTimer)
+  
+    if (this.frontText) this.frontText.style.opacity = 0
+    if (this.backText) this.backText.style.opacity = 0
+  
+    this.flipCard.classList.toggle('is-active', this.isFlipped)
+  
+    this._flipTimer = setTimeout(() => {
+      const target = this.isFlipped ? this.backText : this.frontText
+      if (target) {
+        target.textContent = this.data[this.consumeIndex()]
+        target.style.opacity = 1
+      }
+    }, 600)
+  }
+  
+  
+}
+
+new IsoCard()
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import './../styles/main.scss'
 import { DeckSlider } from './DeckSlider'
 import { GridTextRevealer } from './GridTextRevealer';
+import { IsoCard } from './IsoCard';
 
 
 
 
 new DeckSlider();
 new GridTextRevealer();
+new IsoCard()
